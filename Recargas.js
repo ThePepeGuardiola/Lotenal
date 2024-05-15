978d04b3cffc1695a2836dbcf5af26667ae8f6db
ThePepeGuardiola <acosta.josue.ismael@gmail.com>
2024-05-14 20:43:20 -0400

    Add files via upload

---
+++ Recargas.js
@@ -0,0 +1,14 @@
+var btn = document.getElementById("btn");
+var check = document.getElementById("check");
+select();
+getElementById(select());
+
+btn.addEventListener ("click", select(), {
+    if(){
+        check.style.visibility == "visible"
+    }
+    
+    else {
+        check.style.visibility == "hidden"
+    }
+})
